95ecb17ceb514078087c593b91ec499081e9cf8e
Dionisio Santos <dbnvides@gmail.com>
2020-09-15 15:45:33 -0300

    Project created

---
+++ backend/src/main/java/com/dbstustdio/search/DbseachApplication.java
@@ -0,0 +1,13 @@
+package com.dbstustdio.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbseachApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DbseachApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/dbstustdio/search/DbseachApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dbstustdio.search;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbseachApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

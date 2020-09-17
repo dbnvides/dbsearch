dc245c97b662b2789ed88416d1919da44f8dee9c
Dionisio Santos <dbnvides@gmail.com>
2020-09-17 10:37:27 -0300

    Limpando a estrutura do projeto

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,25 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Quem é ouro no lol é viado!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello world semana dev superior</h1>
   );
 }
 

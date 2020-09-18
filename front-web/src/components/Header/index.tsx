29abc843115b98edfbf8e1351a751cb5df8990f6
Dionisio Santos <dbnvides@gmail.com>
2020-09-18 14:32:12 -0300

    Criando navegação com rotas

--- front-web/src/components/Header/index.tsx
+++ front-web/src/components/Header/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import { ReactComponent as Logo } from '../../assets/logo.svg'; 
+import {Link} from 'react-router-dom';
 import './styles.css';
 const Header = () => (
     <header className="main-header">
         <Logo />
+        <Link to = "/">
+        </Link>
         <div className="logo-text">
             <span className="logo-text-1">Big Game</span>
             <span className="logo-text-2"> Survey</span>
--- front-web/src/pages/Home/index.tsx
+++ front-web/src/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { ReactComponent as ArrowIcon } from '../../assets/arrow.svg';
 import { ReactComponent as GamerImagen } from '../../assets/gamer.svg';
+import {Link} from 'react-router-dom';
 import './styles.css';
 
 const Home = () => (
@@ -12,6 +13,7 @@ const Home = () => (
           <h3 className="home-text-subtitle">
             Clique no botão abaixo e saiba quais são os jogos que os gamer estão escolhendo! 
           </h3> 
+          <Link to="/records">
          <div className="home-actions">
              <button className="home-btn">
              QUERO SABER QUAIS SÃO
@@ -19,7 +21,8 @@ const Home = () => (
              <div className="home-btn-icon">
                 <ArrowIcon/>
              </div>
-         </div>   
+         </div> 
+         </Link>  
       </div>
       <GamerImagen className="home-image"/>
     </div>
